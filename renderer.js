a786b961ef6e965992f8b86276fabb7ca5f83bb5
Jens <jens.jens@live.dk>
2022-11-03 21:32:50 +0100

    feat(debug): more logging for dates and times

--- renderer.js
+++ renderer.js
@@ -55,7 +55,7 @@ const schedule = [
     new Day("Monday", [new Lecture("Advanced Signal Processing", 10, 2)]),
     new Day("Tuesday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
     new Day("Wednesday", [new Lecture("Requirements and Specification for Software Systems", 8, 2)]),
-    new Day("Thursday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
+    new Day("Thursday", [new Lecture("Advanced Signal Processing", 8, 13)]),
     new Day("Friday", [new Lecture("Deep Learning", 8, 3)]),
     new Day("Saturday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)])
 ]
@@ -77,22 +77,31 @@ const hr_to_ms = (hr) => {
 const update = () => {
     const now_date = new Date()
     const now_time = now_date.getTime()
+
+    // update timer
     timer = now_time - start_time
     replace_text('timer', Math.floor(timer / 1000))
-
     console.log("now_time: " + now_time)
+    console.log("start_time ms: " + start_date.getHours())
 
     // set text in the DOM id 'lecture' to the current lecture
     let current_lecture = null
     for (let i = 0; i < schedule[day].lectures.length; i++) {
         const start_time_lecture = hr_to_ms(schedule[day].lectures[i].start_time)
         const runtime_lecture = hr_to_ms(schedule[day].lectures[i].expected_runtime)
-
-        console.log("start_time: " + start_time)
+        const end_time_lecture = start_time_lecture + runtime_lecture
+        
+        // debugging
+        console.log("start_time_lecture: " + start_time)
         console.log("runtime_lecture: " + runtime_lecture)
+        console.log("endtime_lecture: " + end_time_lecture)
+
+        console.log("over start time: " + (now_time > start_time_lecture))
+        console.log("under end time: " + (now_time < end_time_lecture))
+        console.log("now_time - end_time_lecture: " + (now_time - end_time_lecture))
 
         if (now_time > start_time_lecture &&
-            now_time < start_time_lecture + runtime_lecture) {
+            now_time < end_time_lecture) {
             current_lecture = i
             console.log("FOUND LECTURE = " + current_lecture)
             break
