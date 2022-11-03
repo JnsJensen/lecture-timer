09b5fb0f11aa79b2e7c5452e340ffd2dd7bb5e54
jens <jens@jens-desktop.localdomain>
2022-11-03 09:06:20 +0100

    feat(debug): console logging

--- renderer.js
+++ renderer.js
@@ -6,6 +6,14 @@
  * to expose Node.js functionality from the main process.
  */
 
+// const con = require('electron').remote.getGlobal('console')
+// con.log('This will be output to the main process console.')
+
+// const remote = require('electron').remote;
+// const app = remote.app;
+
+// app.console.log('This will output to the main process console.');
+
 // lecture class containing start time and expected runtime of the lecture
 class Lecture {
     constructor(name, start_time, expected_runtime) {
@@ -33,19 +41,37 @@ let countdown = 0
 // const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 // const lectures = [[[0, 0]], [[10, 2]], [[8, 3], [12, 3]], [[8, 2]], [[0, 0]], [[8, 3]], [[0, 0]]]
 
+// const schedule = [
+//     new Day("Sunday", []),
+//     new Day("Monday", [new Lecture("Advanced Signal Processing", 10, 2)]),
+//     new Day("Tuesday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
+//     new Day("Wednesday", [new Lecture("Requirements and Specification for Software Systems", 8, 2)]),
+//     new Day("Thursday", []),
+//     new Day("Friday", [new Lecture("Deep Learning", 8, 3)]),
+//     new Day("Saturday", [])
+// ]
 const schedule = [
-    new Day("Sunday", []),
+    new Day("Sunday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
     new Day("Monday", [new Lecture("Advanced Signal Processing", 10, 2)]),
     new Day("Tuesday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
     new Day("Wednesday", [new Lecture("Requirements and Specification for Software Systems", 8, 2)]),
-    new Day("Thursday", []),
+    new Day("Thursday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)]),
     new Day("Friday", [new Lecture("Deep Learning", 8, 3)]),
-    new Day("Saturday", [])
+    new Day("Saturday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)])
 ]
 
 const replace_text = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
+}
+
+const append_text = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText += text
+}
+
+const hr_to_ms = (hr) => {
+    return hr * 60 * 60 * 1000
 }
 
 const update = () => {
@@ -54,11 +80,21 @@ const update = () => {
     timer = now_time - start_time
     replace_text('timer', Math.floor(timer / 1000))
 
+    console.log("now_time: " + now_time)
+
     // set text in the DOM id 'lecture' to the current lecture
     let current_lecture = null
     for (let i = 0; i < schedule[day].lectures.length; i++) {
-        if (now_time > schedule[day].lectures[i].start_time * 60 * 60 * 1000 && now_time < (schedule[day].lectures[i].start_time + schedule[day].lectures[i].expected_runtime) * 60 * 60 * 1000) {
+        const start_time_lecture = hr_to_ms(schedule[day].lectures[i].start_time)
+        const runtime_lecture = hr_to_ms(schedule[day].lectures[i].expected_runtime)
+
+        console.log("start_time: " + start_time)
+        console.log("runtime_lecture: " + runtime_lecture)
+
+        if (now_time > start_time_lecture &&
+            now_time < start_time_lecture + runtime_lecture) {
             current_lecture = i
+            console.log("FOUND LECTURE = " + current_lecture)
             break
         }
     }
