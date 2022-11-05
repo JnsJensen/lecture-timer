250d81fcb7dcdc886e18820254b55377678ee47d
Jens <jens.jens@live.dk>
2022-11-05 13:26:42 +0100

    feat(style): introduced neumorphism, text replacement animation

--- renderer.js
+++ renderer.js
@@ -35,9 +35,12 @@ class Day {
 const start_date = new Date()
 let start_of_day = new Date()
 start_of_day.setHours(0, 0, 0, 0)
+
 const start_time = start_date.getTime()
 const day = start_date.getDay()
-let countdown = 0
+
+let on_break = false
+let animation_delay = 20
 
 // const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 // const lectures = [[[0, 0]], [[10, 2]], [[8, 3], [12, 3]], [[8, 2]], [[0, 0]], [[8, 3]], [[0, 0]]]
@@ -58,7 +61,7 @@ const schedule = [
     new Day("Wednesday", [new Lecture("Requirements and Specification for Software Systems", 8, 2)]),
     new Day("Thursday", [new Lecture("Advanced Signal Processing", 8, 13)]),
     new Day("Friday", [new Lecture("Deep Learning", 20, 3)]),
-    new Day("Saturday", [new Lecture("Advanced Signal Processing", 8, 3), new Lecture("Wireless Sensor Networks", 12, 3)])
+    new Day("Saturday", [new Lecture("Advanced Signal Processing", 8, 4), new Lecture("Wireless Sensor Networks", 12, 4)])
 ]
 
 const replace_text = (selector, text) => {
@@ -71,9 +74,43 @@ const append_text = (selector, text) => {
     if (element) element.innerText += text
 }
 
+// replace text function, removing the old text one character at a time with a delay, then entering the new text one character at a time with a delay
+const replace_text_animated = (selector, text, delay) => {
+    const element = document.getElementById(selector)
+    if (element) {
+        let old_text = element.innerText
+        let new_text = text
+        let i = 0
+        let j = 0
+        let interval = setInterval(() => {
+            if (i < old_text.length) {
+                element.innerText = old_text.slice(0, old_text.length - i)
+                i++
+            } else if (j <= new_text.length) {
+                element.innerText = new_text.slice(0, j)
+                j++
+            } else {
+                clearInterval(interval)
+            }
+        }, delay)
+    }
+}
+
+const add_event_listener = (selector, event, callback) => {
+    const element = document.getElementById(selector)
+    if (element) element.addEventListener(event, callback)
+}
+
+const set_background_color = (selector, color) => {
+    const element = document.getElementById(selector)
+    if (element) element.style.backgroundColor = color
+}
+
 const hr_to_ms = (hr) => {
     return hr * 60 * 60 * 1000
 }
+
+let previous_lecture_idx = null
 
 const update = () => {
     const now_date = new Date()
@@ -114,7 +151,10 @@ const update = () => {
     // console.log("FOUND LECTURE = " + current_lecture)
     current_lecture = current_lecture_idx !== null ? schedule[day].lectures[current_lecture_idx] : null
     if (current_lecture !== null) {
-        replace_text('lecture', current_lecture.name)
+        if (previous_lecture_idx !== current_lecture_idx) {
+            replace_text_animated('lecture', current_lecture.name, animation_delay)
+            previous_lecture_idx = current_lecture_idx
+        }
 
         let start_of_lecture_date = new Date()
         start_of_lecture_date.setHours(current_lecture.start_time, 0, 0, 0)
@@ -124,12 +164,13 @@ const update = () => {
         // print hours, minutes and seconds in dom timer
         replace_text('timer', timer_date.toTimeString().slice(0, 8))
 
-        console.log("start_of_lecture_date: " + start_of_lecture_date)
-        console.log("now_date: " + now_date)
-        console.log("timer: " + timer_date)
+        // console.log("start_of_lecture_date: " + start_of_lecture_date)
+        // console.log("now_date: " + now_date)
+        // console.log("timer: " + timer_date)
 
         // update expected runtime
-        const expected_runtime_date = new Date(hr_to_ms((current_lecture.expected_runtime) * 0.75 - 1))
+        let lecture_runtime_no_break_ms = hr_to_ms((current_lecture.expected_runtime) * 0.75 - 1)
+        const expected_runtime_date = new Date(lecture_runtime_no_break_ms)
         replace_text('expected_time_modules', current_lecture.expected_runtime)
         replace_text('expected_time_total', expected_runtime_date.toTimeString().slice(0, 8))
 
@@ -146,12 +187,48 @@ const update = () => {
         replace_text('timer', '00:00:00')
     }
 
+    console.log("on_break = " + on_break)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     // set text in the DOM id 'day' to the current day
     replace_text('day', schedule[day].name)
 
+    // add callback to break button 'btn_break' to start break
+    add_event_listener('btn_break', 'click', () => {
+        on_break = !on_break
+        if (on_break) {
+            replace_text_animated('btn_break', 'End Break', animation_delay)
+            replace_text_animated('lecture_status', 'On Break', animation_delay)
+            set_background_color('status_icon', 'crimson')
+        } else {
+            replace_text_animated('btn_break', 'Start Break', animation_delay)
+            replace_text_animated('lecture_status', 'Lecture in Progress', animation_delay)
+            set_background_color('status_icon', 'limegreen')
+        }
+    })
+
+    add_event_listener('slide_increment', 'click', () => {
+        const element = document.getElementById('total_slide')
+        let total_slides = 0
+        if (element) {
+            total_slides = parseInt(element.value)
+            total_slides += 1
+            element.value = total_slides
+        }
+    })
+
+    add_event_listener('slide_decrement', 'click', () => {
+        const element = document.getElementById('total_slide')
+        let total_slides = 0
+        if (element) {
+            total_slides = parseInt(element.value)
+            if (total_slides > 0) {
+                total_slides -= 1
+                element.value = total_slides
+            }
+        }
+    })
     
     update()
     setInterval(update, 1000)
